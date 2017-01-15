f667ead2a7032cc47e5d1d38f443b914aa025178
Enrik <enrik@Enriks-MacBook-Air.local>
2017-01-15 14:45:10 +0700

    update recent

---
+++ FreeComic/Model/RecentStory.swift
@@ -0,0 +1,28 @@
+//
+//  RecentStory.swift
+//  FreeComic
+//
+//  Created by Enrik on 1/15/17.
+//  Copyright © 2017 Enrik. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RecentGenre: Object {
+    dynamic var id: String = ""
+}
+
+class RecentStory: Object {
+    dynamic var id = ""
+    dynamic var name: String = ""
+    dynamic var author: String = ""
+    dynamic var thumbUrl: String = ""
+    dynamic var numberOfChap: String = ""
+    dynamic var rank: String = ""
+    dynamic var dataImage: NSData? = nil
+    dynamic var chapterId: String = ""
+    dynamic var chapterName: String = "" 
+    dynamic var time: String = ""
+    let genre = List<RecentGenre>()
+}
---
+++ FreeComic/View/RecentTableCell/RecentTableCell.swift
@@ -0,0 +1,48 @@
+//
+//  RecentTableCell.swift
+//  FreeComic
+//
+//  Created by Enrik on 1/15/17.
+//  Copyright © 2017 Enrik. All rights reserved.
+//
+
+import UIKit
+
+class RecentTableCell: UITableViewCell {
+
+    @IBOutlet weak var thumbView: UIImageView!
+    
+    @IBOutlet weak var labelName: UILabel!
+    
+    @IBOutlet weak var labelAuthor: UILabel!
+    
+    @IBOutlet weak var labelNumberChapter: UILabel!
+    
+    @IBOutlet weak var labelTime: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+    func configure(_ recentStory: RecentStory) {
+        if let data = recentStory.dataImage {
+            self.thumbView.image = UIImage(data: data as Data)
+        } else {
+            let url = URL(string: "http://" + recentStory.thumbUrl)
+            self.thumbView.kf.setImage(with: url, placeholder: UIImage(named: "no_thumbnail"), options: nil, progressBlock: nil, completionHandler: nil)
+        }
+        
+        self.labelName.text = recentStory.name
+        self.labelAuthor.text = recentStory.author
+        self.labelNumberChapter.text = recentStory.numberOfChap
+        self.labelTime.text = recentStory.time
+  
+    }
+}
